9922653a36d909eda8f85cff7d2da8038f3d335e
Robert Prije <robert@prije.family>
2019-09-26 07:56:14 +1000

    Fail on no args

--- default.nix
+++ default.nix
@@ -4,5 +4,7 @@ pkgs.writeScriptBin "my-app" ''
   set -euo pipefail
   SECRET=''${1:-}
 
+  [ -n "$SECRET" ] || (echo "Usage: $0 SECRET_FILE" && exit 1)
+
   echo "Running with secret: $(cat $SECRET)"
 ''
